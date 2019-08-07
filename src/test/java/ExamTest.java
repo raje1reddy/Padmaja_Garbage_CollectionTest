9bb44537e8c100663a538f54b664a93c7e8c2e77
unknown <raje1reddy@gmail.com>
2019-08-07 19:33:08 +0100

    Garbage collection of referenced objects

---
+++ src/test/java/ExamTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+public class ExamTest {
+
+    @org.junit.Test
+
+        public void myMethod(){
+            int result = 88;
+            if (result > 78) {
+                Exam myExam1 = new Exam();
+                myExam1.setName("Android");
+            }
+            else {
+                Exam myExam2 = new Exam();
+                myExam2.setName("MySQL");
+            }
+        }
+
+
+    }
