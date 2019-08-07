e0ceb916452fd3bf4bc86057d3e9e7850adffee9
unknown <raje1reddy@gmail.com>
2019-08-07 19:49:09 +0100

    Garbage collecting referenced objects

--- src/main/java/Exam.java
+++ src/main/java/Exam.java
@@ -1,11 +1,15 @@
 
 
-public class Exam {
-    String name;
-
-    public void setName(String newName) {
-        name = newName;
+class Exam {
+    private String name;
+    private Exam other;
+    public Exam(String name) {
+        this.name = name;
         System.out.println(name);
+    }
+    public void setExam(Exam exam) {
+        other = exam;
+        System.out.println(other);
     }
 }
 
--- src/test/java/ExamTest.java
+++ src/test/java/ExamTest.java
@@ -1,20 +1,22 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ExamTest {
 
-    @org.junit.Test
+    Exam php = new Exam("PHP");
+    Exam java = new Exam("Java");
 
-        public void myMethod(){
-            int result = 88;
-            if (result > 78) {
-                Exam myExam1 = new Exam();
-                myExam1.setName("Android");
-            }
-            else {
-                Exam myExam2 = new Exam();
-                myExam2.setName("MySQL");
-            }
-        }
+    @Test
+    public void setExam() {
+        php.setExam(java);
+        java.setExam(php);
 
-
+        php = null;
+        java = null;
     }
+
+}
+
+
+
